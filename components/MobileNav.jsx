972c2f2b13f53a2cfadea1416da67a0dd78a5cea
TejalAGunjal <tejal.a.gunjal@gmail.com>
2025-01-22 22:37:09 -0500

    MobileNav and sheet.jsx file updated

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -10,10 +10,37 @@ const links = [
     name: "home",
     path: "/",
   },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
 ];
 
 const MobileNav = () => {
-  return <nav>MobileNav</nav>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-purple-600" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        {/*logo  */}
+        <div>logo</div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
