ff5fc4c7120e9d1974d19d982d20df61dc4fefb4
TejalAGunjal <tejal.a.gunjal@gmail.com>
2025-01-29 22:36:15 -0500

    Button and fidownload icon used in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -9,15 +9,23 @@ const Home = () => {
           {/* text */}
           <div className="text-center xl:text-left">
             <span className="text-xl">Software Developer</span>
-            <h1 className="h1">
+            <h1 className="h1 mb-6">
               Hello I'm
               <br />
-              <span>Tejal</span>
+              <span className="text-purple-700">Tejal Gunjal</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               {" "}
               I am Software Engineer and QA. I am profient in various languages
             </p>
+
+            {/* button and socials */}
+            <div>
+              <Button>
+                <span>Download CV</span>
+                <FiDownload />
+              </Button>
+            </div>
           </div>
 
           {/* photos */}
